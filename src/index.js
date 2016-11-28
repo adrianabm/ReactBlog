3e2d9f02d03650d722072fa74a821b0ca839ed06
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-28 16:09:17 +0100

    Middleware and fetch posts action

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
 import routes from './routes'
+// Middleware
+import promise from 'redux-promise'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
