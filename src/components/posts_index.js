287cf8ea2f1d051ad9d215217174ccb770466578
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-28 16:44:44 +0100

    Posts index component is connected to the fetch posts action

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchPosts } from '../actions/index'
 
-export default () => {
-  return <div>List of blog posts</div>
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts()
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    )
+  }
 }
+
+// In ES6, it's possible to skip this step of writing this function and just do this in the export:
+// export default connect(null, { fetchPosts })(PostsIndex)
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex)
