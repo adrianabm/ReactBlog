3d0c57625c72fe94fc1c4d8640249eafb3c9279a
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-29 11:07:28 +0100

    List of posts is shown on index page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,13 +9,27 @@ class PostsIndex extends Component {
     this.props.fetchPosts()
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <strong>{ post.title }</strong>
+          <span className="post-category">{ post.categories }</span>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="text-xs-right">
           <Link to="/posts/new" className="btn btn-primary">Add a post</Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          { this.renderPosts() }
+        </ul>
       </div>
     )
   }
@@ -25,7 +39,11 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchPosts }, dispatch)
 }
 
-export default connect(null, mapDispatchToProps)(PostsIndex)
+function mapStateToProps(state) {
+  return { posts: state.posts.all }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex)
 
 // In ES6, it's possible to skip this step of writing the mapDispatchToProps function and just write the export as the following:
 // export default connect(null, { fetchPosts })(PostsIndex)
