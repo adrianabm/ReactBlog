1dfccfbabee7109c24a6c911d820aeb42e90c11d
Adriana Matsufuji <adrianabm@gmail.com>
2018-07-26 11:36:13 +0200

    Update routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, browserHistory } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
-import routes from './routes'
+// import routes from './routes'
 // Middleware
 import promise from 'redux-promise'
 
-const createStoreWithMiddleware = applyMiddleware(
-  promise
-)(createStore)
+import App from './components/app'
+import PostsIndex from './components/posts_index'
+import PostsShow from './components/posts_show'
+import PostsNew from './components/posts_new'
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={ browserHistory } routes={ routes } />
-  </Provider>
-  , document.querySelector('.container'))
+    <BrowserRouter>
+      <Switch>
+        <Route path="/posts/new" component={PostsNew} />
+        <Route path="/posts/:id" component={PostsShow} />
+        <Route path="/" component={PostsIndex} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.container')
+)
